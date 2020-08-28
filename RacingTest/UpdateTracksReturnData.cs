a7852404dc55d7343a05114461afee6b16ddeae7
James McKee <jamesomckee@icloud.com>
2020-08-28 22:21:44 +0100

    Hello world!

---
+++ RacingTest/UpdateTracksReturnData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Windows.Media;
+
+namespace RacingTest
+{
+	/// <summary>
+	/// Used to return the information required to create a new tyre track
+	/// </summary>
+	public class UpdateTracksReturnData
+	{
+		public Vector2 Position { get; set; }
+		public Vector2 LastLinePos { get; set; }
+		public Vector2 WheelDirection { get; set; }
+		public float WheelMovementAngle { get; set; }
+		public string LnName { get; set; }
+		public Brush TrailBrush { get; set; }
+
+		public UpdateTracksReturnData(Vector2 position, Vector2 lastLinePos, Vector2 wheelDirection, float wheelMovementAngle, string lnName, Brush trailBrush)
+		{
+			Position = position;
+			LastLinePos = lastLinePos;
+			WheelDirection = wheelDirection;
+			WheelMovementAngle = wheelMovementAngle;
+			LnName = lnName;
+			TrailBrush = trailBrush;
+		}
+	}
+}
